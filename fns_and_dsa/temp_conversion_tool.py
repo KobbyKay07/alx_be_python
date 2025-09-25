25481ca8bdcd2ea110a7b5182cd86bc8f49a0f11
Benjamin Kesse Ampadu <ampadubenjaminkesse@gmail.com>
2025-09-25 20:36:04 +0000

    Temperature converter

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,8 +1,8 @@
 global  FAHRENHEIT_TO_CELSIUS_FACTOR
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5.0 / 9.0
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 
 global  CELSIUS_TO_FAHRENHEIT_FACTOR
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9.0 / 5.0
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 temperature = int(input("Enter the temperature to convert: "))
 unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
